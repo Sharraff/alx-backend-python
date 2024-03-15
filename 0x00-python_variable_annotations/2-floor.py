d5aa8853f85796ea7b4b6a0ea4008073ec98b51c
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-03-15 08:37:29 +0000

    python anotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Basic annotations - floor """
+
+
+def floor(n: float) -> int:
+  """ Floor Function"""
+  return int(n)
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Complex types - functions """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+  """ Return function that multiplies float by multiplier """
+
+  def multiply(num: float) -> float:
+    """ Return product of multiplier and num """
+    return num * multiplier
+
+  return multiply
