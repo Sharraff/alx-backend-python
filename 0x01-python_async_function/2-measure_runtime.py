d722b36b1359a0da0d960c7733b80b11c51e587c
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-03-18 21:20:08 +0000

    checking linter

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Defines a function to measure time
+"""
 import asyncio
 import time
 
@@ -6,17 +9,17 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-  """
-  Return the average time it takes for coroutines to complete
+    """
+    Return the average time it takes for coroutines to complete
 
-  measure_time: run time of opperation
-  Args:
-     n: int
-     max_delay: int
-  return: float
-  """
-  start_time = time.perf_counter()
-  asyncio.run(wait_n(n, max_delay))
-  end_time = time.perf_counter()
-  total_time = start_time + end_time
-  return float(total_time / n)
+    measure_time: run time of opperation
+    Args:
+        n: int
+        max_delay: int
+    return: float
+    """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.perf_counter()
+    total_time = start_time + end_time
+    return float(total_time / n)
