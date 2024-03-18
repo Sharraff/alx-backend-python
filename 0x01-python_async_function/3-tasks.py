cb8b49552f84c4b116e8771a8131190a99d41215
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-03-18 21:02:55 +0000

    checking linter

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,8 +6,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-  """
-  task_wait_random: create task
-  return: asyncio.Task
-  """
-  return asyncio.create_task(wait_random(max_delay))
+    """
+    task_wait_random: create task
+    return: asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
