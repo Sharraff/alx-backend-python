4d64fd5405bc931e28d8c22fb8c5f6974dd24e30
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-03-18 20:40:32 +0000

    checking linter

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,15 +9,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-  """
-  Create coroutines and await their completion upon which
-  they will be added to the list to be returned
+    """
+    Create coroutines and await their completion upon which
+    they will be added to the list to be returned
 
-  wait_n: run concurrent tasks
-  Args:
-      n: int
-      max_delay: int
-  return: list
-  """
-  tasks = [wait_random(max_delay) for _ in range(n)]
-  return [await task for task in asyncio.as_completed(tasks)]
+    wait_n: run concurrent tasks
+    Args:
+         n: int
+         max_delay: int
+    return: list
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
