97c6b0d2ea7bce5f1cd3a22d1395bd1fae1aafb6
Sharraff <111119433+Sharraff@users.noreply.github.com>
2024-03-18 21:24:43 +0000

    checking linter

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -20,6 +20,5 @@ def measure_time(n: int, max_delay: int) -> float:
     """
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.perf_counter()
-    total_time = start_time + end_time
+    total_time = time.perf_counter() - start_time
     return float(total_time / n)
